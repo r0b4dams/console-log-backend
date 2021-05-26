413ed5d7da86104c28a1dda1c1f516d568d877b2
Robert Adams <adamsiii.robert@gmail.com>
2021-05-26 12:00:32 -0700

    comments added

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const authRoutes = require('./auth')
 
-router.use('/auth', authRoutes);
-router.use('/api', apiRoutes);
+router.use('/auth', authRoutes); // user auth routes
+router.use('/api', apiRoutes);   // api routes
 
 module.exports = router;
 
