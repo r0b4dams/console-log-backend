106222cd8235c914b6073eef0fd8c95e18bef974
Robert Adams <adamsiii.robert@gmail.com>
2021-05-27 08:48:38 -0700

    added basic validation to user model

--- models/User.js
+++ models/User.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    trim: true,
+    required: "Username is Required"
+  },
+
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [({ length }) => length >= 6, "Password should be longer."]
+  },
+  
   // need an array of favorite walkthrough IDs
   favs: [
     {
