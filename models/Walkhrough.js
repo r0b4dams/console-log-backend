4ee1588b70c776fc1e472699980c7b13b54382f4
Robert Adams <adamsiii.robert@gmail.com>
2021-05-26 19:18:36 -0700

    adjusted model to include custom timestamps

--- models/Walkhrough.js
+++ models/Walkhrough.js
@@ -4,15 +4,21 @@ const Schema = mongoose.Schema;
 
 const WalkthroughSchema = new Schema({
   title: String,
-  gameID: String,
+  gameID: Number,
   gameName: String,
   Content: String,
   link: String,
-  rating: Number,
+  rating: Number, // default should be zero
+
   user_id:{
     type: Schema.Types.ObjectId,
     ref: "User"
-  }
+  },
+
+},
+
+{ timestamps: { createdAt: 'created',
+                updatedAt: 'updated' }
 });
 
 const Walkthrough = mongoose.model("Walkthrough", WalkthroughSchema);
