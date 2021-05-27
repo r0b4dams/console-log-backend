9f3af982af97bd0d916dd884fdae2f4395ac0737
Robert Adams <adamsiii.robert@gmail.com>
2021-05-26 19:16:23 -0700

    added signup route

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -12,13 +12,38 @@ router.get("/", async (req, res) => {
 
 // SIGNUP
 // Create a new user
+// send the following format
+// need to figure out how to hash (bcrypt?)
+/*
+res.body should mimic the following format:
+{
+	"username":"Tiki",
+	"password":"kitteh"
+}
+*/
+// this works; need to hash pw
+// // localhost:3001/auth/signup
+router.post("/signup", async ({body}, res) => {
+    try {
+        newUser = await db.User.create(body);
+        console.log(newUser);
+      }
+      catch(err) {
+        res.json(err);
+      }
+  });
 
 // LOGIN
+// a user logs in
+// localhost:3001/auth/login
+router.post("/login", (req,res)=>{
+    res.json("logIN route");
+});
 
-// LOGOUT
-
-
-
-
+// a user logs out
+// localhost:3001/auth/logout
+router.post("/logout", (req,res) => {
+    res.json("logOUT route");
+});
 
 module.exports = router;
