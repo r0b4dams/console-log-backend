9d38b4f5ea788e95084d037543f806ddba169fb6
Robert Adams <adamsiii.robert@gmail.com>
2021-05-27 10:37:14 -0700

    linked bcrypt and added token to signup route

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const db = require("../../models");
 
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const tokenAuth = require("../../middleware/tokenAuth");
 
 // localhost:3001/auth/
-router.get("/", async (req, res) => {
+router.get("/", tokenAuth, async (req, res) => {
     try {
         res.json("auth routes linked!")
     } catch (err) {
@@ -25,12 +24,20 @@ res.body should mimic the following format:
 	"password":"kitteh"
 }
 */
-// this works; need to hash pw
-// // localhost:3001/auth/signup
+
+// localhost:3001/auth/signup
 router.post("/signup", async ({body}, res) => {
     try {
         newUser = await db.User.create(body);
-        res.status(200).json(newUser);
+        const token = jwt.sign({
+            username: newUser.username,
+            id: newUser._id
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"2h"
+        })
+        res.json({token, user:newUser })
       }
       catch(err) {
         res.json(err);
@@ -40,14 +47,68 @@ router.post("/signup", async ({body}, res) => {
 // LOGIN
 // a user logs in
 // localhost:3001/auth/login
-router.post("/login", (req,res)=>{
-    res.json("logIN route");
+router.post("/login", async (req,res)=>{
+
+    console.log(req.body);
+
+    // res.json("logIN route");
+
+    // find a user by username
+        // if no user, => user not found
+        // else if user found, but (!bcrypt.compareSync(req.body.password, user.password)) => auth failed
+        // else create a token and send it to client
+
+    try { 
+
+        // const loginUser = await db.User.findOne({ username: req.body.username });
+        // console.log(loginUser);
+
+        // if (!loginUser) {
+        //     console.log('user not found')
+        //     return res.status(403).json({ message: "auth failed" });
+        // }
+
+        // res.status(200).json(loginUser);
+        res.json(req.body);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+    // User.findOne({
+    //     where: {
+    //         email: req.body.email
+    //     }
+    // }).then(user => {
+    //     if (!user) {
+    //         console.log('user not found')
+    //         return res.status(403).json({ message: "auth failed" })
+    //     } else if (!bcrypt.compareSync(req.body.password, user.password)) {
+    //         console.log(req.body.password);
+    //         console.log("passwords dont match")
+    //         return res.status(403).json({ message: "auth failed" })
+    //     } else {
+    //         const token = jwt.sign({
+    //             name:user.name,
+    //             email:user.email,
+    //             id:user.id
+    //         },
+    //         process.env.JWT_SECRET,
+    //         {
+    //             expiresIn:"2h"
+    //         })
+    //         res.json({token, user })
+    //     }
+    // })
+
 });
 
 // a user logs out
 // localhost:3001/auth/logout
 router.post("/logout", (req,res) => {
+
     res.json("logOUT route");
+
 });
 
 module.exports = router;
