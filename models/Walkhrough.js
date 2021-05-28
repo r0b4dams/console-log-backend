85886758e6fb919bf6ceb848d0fd382427aee30f
Robert Adams <adamsiii.robert@gmail.com>
2021-05-28 11:51:33 -0700

    reorganized model, added backround image column

--- models/Walkhrough.js
+++ models/Walkhrough.js
@@ -3,31 +3,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WalkthroughSchema = new Schema({
-  title: String,
-  game_id: {
-    type:Number,
-    required: "Game ID is Required"
-  },
-  gameName: String,
-  Content: String,
-  link: String,
 
-  rating: {
-    type: Number,
-    default: 0
-  },
+  title: String,
+  content: String,
+  link: String,
 
   user_id:{
     type: Schema.Types.ObjectId,
     ref: "User",
-    required: "User ID is Required"
+    required: "user ID is required"
   },
 
+  game_id: {
+    type:Number,
+    required: "game ID is required"
+  },
+  gameName: {
+    type: String,
+    required: "game name is required"
+  },
+  gameImgLink: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
 },
 
-{ timestamps: { createdAt: 'created',
-                updatedAt: 'updated' }
-});
+  { timestamps: { createdAt: 'created',
+                  updatedAt: 'updated' }
+  }
+);
 
 const Walkthrough = mongoose.model("Walkthrough", WalkthroughSchema);
 
