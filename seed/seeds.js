4dc09877fe38fb3b66001bc8c65ea53c3a449420
Robert Adams <adamsiii.robert@gmail.com>
2021-06-02 10:27:32 -0700

    renamed db in seed

--- seed/seeds.js
+++ seed/seeds.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/proj3db", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/consolelogdb", {
      useNewUrlParser: true ,
      useUnifiedTopology: true
 });
